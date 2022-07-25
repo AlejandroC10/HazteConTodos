d2c9619dffe60ed9e0d8bca55962b3a201b4c293
AlejandroC10 <106809387+AlejandroC10@users.noreply.github.com>
2022-07-25 12:38:12 +0200

    [test] Added DeletePokemonById and ReadPokemonReturnAPokemonList

--- Infrastructure.Tests/PokemonDbShould.cs
+++ Infrastructure.Tests/PokemonDbShould.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Domain;
+using FluentAssertions;
 using Xunit;
 
 namespace Infrastructure.Tests;
@@ -5,7 +11,29 @@ namespace Infrastructure.Tests;
 public class PokemonDbShould
 {
     [Fact]
-    public void Test1()
+    public void ReadPokemonReturnAPokemonList()
     {
+        var pokemonDb = new PokemonDb();
+        var pokemonList = pokemonDb.ReadPokemon();
+        
+        var path = AppDomain.CurrentDomain.BaseDirectory;
+        var jsonContent = File.ReadAllText(Path.Combine(path, "pokedex.json"));
+        var expectedList = JsonSerializer.Deserialize<List<Pokemon>>(jsonContent);
+
+        pokemonList.Should().BeEquivalentTo(expectedList);
     }
+    
+    [Fact]
+    public void DeletePokemonById()
+    {
+        var pokemonDb = new PokemonDb();
+        var pokemonList = pokemonDb.ReadPokemon();
+        var pokemon = pokemonList.Find(pokemon => pokemon.Id == 1);
+        
+        pokemonDb.DeletePokemon(1);
+        var finalList = pokemonDb.ReadPokemon();
+        
+        finalList.Should().NotContain(pokemon);
+    }
+
 }
