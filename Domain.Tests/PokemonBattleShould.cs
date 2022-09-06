fc731faa7dd1c8a3eb3cbbc872764c34e4efc980
cmedina-voxel <106809488+cmedina-voxel@users.noreply.github.com>
2022-09-06 10:43:56 +0200

    [test] Added LoadBattleFromJson

--- Domain.Tests/PokemonBattleShould.cs
+++ Domain.Tests/PokemonBattleShould.cs
@@ -132,4 +132,24 @@ public class PokemonBattleShould
         var path = AppDomain.CurrentDomain.BaseDirectory;
         File.Exists(Path.Combine(path, $"{pokemon.Id}vs{pokemon2.Id}.json")).Should().BeFalse();
     }
+    
+    [Fact]
+    public void LoadBattleFromJson()
+    {
+        var pokemon = pokemonList.Find(pokemon => pokemon.Id == 1);
+        var pokemon2 = pokemonList.Find(pokemon => pokemon.Id == 2);
+        
+        var initialPokemonBattle = new PokemonBattle();
+        initialPokemonBattle.CreateBattle(pokemon, pokemon2);
+        initialPokemonBattle.Combat();
+        initialPokemonBattle.SaveBattle(); 
+        
+        var pokemonBattle = new PokemonBattle();
+        pokemonBattle.CreateBattle(pokemon, pokemon2);
+
+        var path = AppDomain.CurrentDomain.BaseDirectory;
+        var jsonContent = File.ReadAllText(Path.Combine(path, $"{pokemon.Id}vs{pokemon2.Id}.json"));
+        var expectedBattle = JsonSerializer.Deserialize<PokemonBattle>(jsonContent);
+        pokemonBattle.Should().BeEquivalentTo(expectedBattle);
+    }
 }
