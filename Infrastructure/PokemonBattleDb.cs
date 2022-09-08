11652d6645a5465d9963cfc7caaca36784d8d6c9
cmedina-voxel <106809488+cmedina-voxel@users.noreply.github.com>
2022-09-08 12:19:27 +0200

    [feature] Implemented SaveBattleInfoToJson

--- Infrastructure/PokemonBattleDb.cs
+++ Infrastructure/PokemonBattleDb.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using System.Text.Json;
+using Domain;
 
 namespace Infrastructure;
 
@@ -6,6 +7,11 @@ public class PokemonBattleDb: IPokemonBattleDb
 {
     public void SaveBattle(PokemonBattle pokemonBattle)
     {
+        var pokemonOne = pokemonBattle.PokemonBattleInfo.SelectedPokemon[0].Id;
+        var pokemonTwo = pokemonBattle.PokemonBattleInfo.SelectedPokemon[1].Id;
         
+        var jsonContent = JsonSerializer.Serialize<PokemonBattleInfo>(pokemonBattle.PokemonBattleInfo);
+        var path = AppDomain.CurrentDomain.BaseDirectory;
+        File.WriteAllText(Path.Combine(path, $"{pokemonOne}vs{pokemonTwo}.json"), jsonContent);
     }
 }
